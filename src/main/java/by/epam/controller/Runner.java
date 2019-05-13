57759944cbe369b0bfb34123182152cdcde724b9
Vlad <vladislav.kuzmich.96@mail.ru>
2019-05-13 23:52:37 +0300

    добавил логирование

--- src/main/java/by/epam/controller/Runner.java
+++ src/main/java/by/epam/controller/Runner.java
@@ -4,6 +4,7 @@ import by.epam.service.Service;
 import by.epam.service.ServiceException;
 import by.epam.service.ServiceFactory;
 import by.epam.service.ServiceKey;
+import org.apache.log4j.Logger;
 
 import java.util.Scanner;
 
@@ -12,12 +13,14 @@ public class Runner {
     private static String MENU = "press 1 for find employee\npres 2 for sort by salary\npres 3 for sort by experience\npress 4 for create team\npress 0 for exit team";
     private static Service service = ServiceFactory.getInstance().getGeneralService(ServiceKey.GENERAL);
     private static Scanner scanner = new Scanner(System.in);
+    final static Logger logger = Logger.getLogger(Runner.class);
 
     public static void main(String[] args) {
         try {
             service.handle("file.txt");
         } catch (ServiceException e) {
             System.out.println("File reading failed");
+            logger.error(e);
         }
         start();
     }
@@ -41,6 +44,7 @@ public class Runner {
                     System.out.println(service.find(scanner.nextLine(), scanner.nextLine()));
                 } catch (ServiceException e) {
                     System.out.println("Entered wrong find parameters");
+                    logger.error(e);
                 }
                 return true;
             case "2":
